3e6ba18f36e1b58aee4fac546bc0d130e91af636
MariamTakrida <145062139+MariamTak@users.noreply.github.com>
2024-12-05 00:07:50 +0100

    Update firebase.js

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -7,12 +7,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDuhlpjruJT4p0KFUT2wqNyiwLihg9iAAk",
-  authDomain: "taskhub-aa53b.firebaseapp.com",
-  projectId: "taskhub-aa53b",
-  storageBucket: "taskhub-aa53b.firebasestorage.app",
-  messagingSenderId: "782807375531",
-  appId: "1:782807375531:web:18976ed8342ada1ee5a1ee"
+
 };
 
 // Initialize Firebase
